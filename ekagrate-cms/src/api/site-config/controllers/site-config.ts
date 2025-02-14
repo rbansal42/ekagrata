089c3147aec6a85fdf57c8e3c64a56551bfd7302
Rahul Bansal <42.rahulbansal@gmail.com>
2025-02-14 22:35:54 +0530

    Updating CMS to remove unnecessary things

--- ekagrate-cms/src/api/site-config/controllers/site-config.ts
+++
@@ -1,7 +0,0 @@
-/**
- * site-config controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::site-config.site-config'); 
--- ekagrate-cms/src/api/site-config/services/site-config.ts
+++
@@ -1,7 +0,0 @@
-/**
- * site-config service
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreService('api::site-config.site-config'); 
